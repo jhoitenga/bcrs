9300eca8b335f416a8ba7619a3ac30de9eda0cb0
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-11-20 17:58:12 -0600

    login button and drop down changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,13 +5,22 @@
  */
 
 // imports statements
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private cookieService: CookieService, private router: Router) {}
 
+  ngOnInit(): void {}
+
+  // Function to check if a user is logged in.
+  isLoggedIn(): boolean {
+    return !!this.cookieService.get('sessionEmail');
+  }
 }
