6d2e953539a155c1d5e185f4748b601e5917b676
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-11-26 07:04:49 -0600

    employee directory

--- src/app/employee-directory/employee-directory.component.spec.ts
+++ src/app/employee-directory/employee-directory.component.spec.ts
@@ -1,4 +1,13 @@
- import { ComponentFixture, TestBed } from '@angular/core/testing';
+/*<!--
+ * Title: employee-directory.component.spec.ts
+ * Modified By: Michael Christman, Zahava Gopin & Jennifer Hoitenga
+ * Author: Professor Krasso
+ * Date: 11/26/2023
+ * Sources:
+ * BCRS Starter Project: https://github.com/buwebdev/web-450/tree/master/starter-projects/bcrs
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeDirectoryComponent } from './employee-directory.component';
 
@@ -8,7 +17,7 @@ describe('EmployeeDirectoryComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EmployeeDirectoryComponent]
+      declarations: [EmployeeDirectoryComponent],
     });
     fixture = TestBed.createComponent(EmployeeDirectoryComponent);
     component = fixture.componentInstance;
--- src/app/employee-directory/employee-directory.component.ts
+++ src/app/employee-directory/employee-directory.component.ts
@@ -1,10 +1,58 @@
+/*<!--
+ * Title: employee-directory.component.ts
+ * Modified By: Michael Christman, Zahava Gopin & Jennifer Hoitenga
+ * Author: Professor Krasso
+ * Date: 11/26/2023
+ * Sources:
+ * BCRS Starter Project: https://github.com/buwebdev/web-450/tree/master/starter-projects/bcrs
+ */
+
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.interface';
 
 @Component({
   selector: 'app-employee-directory',
   templateUrl: './employee-directory.component.html',
-  styleUrls: ['./employee-directory.component.css']
+  styleUrls: ['./employee-directory.component.css'],
 })
 export class EmployeeDirectoryComponent {
+  errorMessage: string = '';
+  successMessage: string = '';
+  users: any[] = [];
+  isImageLoaded = false;
 
+  constructor(private userService: UserService) {
+    this.users = [];
+    this.errorMessage = '';
+
+    // Find all users
+    this.userService.findAllUsers().subscribe({
+      next: (users: User[]) => {
+        // Filtering out disabled users
+        this.users = users.filter((user: User) => !user.isDisabled);
+      },
+      error: (err) => {
+        // If error, display error message
+        this.errorMessage =
+          'Failed to load employee directory. Please try again later.';
+        //console.log(err);
+      },
+    });
+  }
+
+  // Format role
+  capitalizeFirstLetter(text: string): string {
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+  }
+
+  // Changing isImageLoaded is true for spinner
+  onImageLoad() {
+    this.isImageLoaded = true;
+  }
+
+  // To ensure unique image for each employee
+  getRandomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
